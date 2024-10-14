add88bb2a76029eb1ba73b777b391e8bfbbe1e1d
Abhilesh Dhawanjewar <2447878+abhilesh@users.noreply.github.com>
2024-10-14 15:23:06 +0100

    Update to v0.12.0

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,17 +1,6 @@
 // Initialize medium zoom.
 $(document).ready(function () {
-  var margin = window.innerWidth >= 992 ? window.innerWidth * 0.05 : 0;
-
   medium_zoom = mediumZoom("[data-zoomable]", {
-    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for transparency.
-    margin: margin,
-  });
-
-  // Update margin when window is resized.
-  $(window).resize(function () {
-    var margin = window.innerWidth >= 992 ? window.innerWidth * 0.05 : 0;
-    medium_zoom.update({
-      margin: margin,
-    });
+    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
   });
 });
